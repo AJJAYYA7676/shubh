32977eadabe1ee4387e0456b70a1eb76b14ff802
HP <HP@DESKTOP-8M9HAN5>
2023-07-30 17:08:41 +0530

    hi

--- src/test/java/scripts/test1.java
+++ src/test/java/scripts/test1.java
@@ -1,5 +1,12 @@
 package scripts;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class test1 {
+	@Test
+	public void tc01() {
+		Reporter.log("hello java", true);
+	}
 
 }
