909259567b7855f9f5897c0a8f6ae9dff2e361e1
HP <HP@DESKTOP-8M9HAN5>
2023-07-31 12:35:51 +0530

    hi

---
+++ src/test/java/scripts/test4.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test4 {
+	@Test
+	public void tc04() {
+		Reporter.log("hello manual", true);
+	}
+
+}
